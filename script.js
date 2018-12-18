cda7317af38cf297858a386df37adab63151841f
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-18 08:44:02 +0200

    fixes from Tanya

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 question();
 getSimpleNumbers();
 fizzBuzzNumbers();
-console.log(getLetterCounts('some Text'));
-console.log(getRandomInteger(5, 10));
+console.log(getLetterCounts('Hello man! how are you'));
+console.log(getRandomInteger(0.1, 2));
 console.log(getFibonaccy(3)); 
 console.log(getFibonaccy(7)); 
 console.log(getFibonaccyLoop(3)); 
@@ -13,14 +13,13 @@ function question() {
   var number;
   do {
     number = +prompt('Enter the number more then 100', 0);
-  } while (!number || number < 100 || (!(number === parseInt(number)))) {
+  } while (!number || number < 100 || (!(number === parseInt(number)))) 
     alert('You entered number '+ number);
-  }
 }
 
 function getSimpleNumbers() {
   number:
-  for (var i = 2; i <=10; i++) {
+  for (var i = 2; i <= 10; i++) {
     for (var j = 2; j < i; j++) {
       if (i % j === 0)
       continue number;
@@ -31,11 +30,11 @@ function getSimpleNumbers() {
 
 function fizzBuzzNumbers() {
   for (var i = 1; i <= 100; i++) {
-    if (i % 3 === false && i % 5 === false) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log('FizzBuzz');
-    } else if (i % 5 === false) {
+    } else if (i % 5 === 0) {
       console.log('Buzz');
-    } else if (i % 3 === false) {
+    } else if (i % 3 === 0) {
       console.log('Fizz');
     } else {
       console.log(i);
@@ -44,19 +43,21 @@ function fizzBuzzNumbers() {
 }
 
 function getLetterCounts(text) {
-  var count = text.length;
-  var result = 0;
-  for (var i = 0; i < count; i++) {
-    if (text.charAt(i) === '') {
-      result++;
+  let chars = {};
+  for (let i = 0; i < text.length; ++i) {
+    let char = text[i].toLowerCase();
+    if (char >= 'a' && char <= 'z') {
+      if (chars[char])
+        ++chars[char]
+      else
+        chars[char] = 1;
     }
-    result++;
   }
-return result;
+  return chars;
 }
 
 function getRandomInteger(min, max) {
-  var randomNumber = min - 0.5 + Math.random() * (max - min + 1)
+  var randomNumber = min - 0.1 + Math.random() * (max - min + 1);
   randomNumber = Math.round(randomNumber);
   return randomNumber;
 }
@@ -89,4 +90,3 @@ function reverseString(string) {
   }
   return newString;
 }
-  
